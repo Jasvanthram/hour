2083b6591b93893f166481eae1663769718b9cb2
unknown <jasvanthram10@gmil.com>
2023-05-11 16:39:13 +0530

    new changes

---
+++ test.js
@@ -0,0 +1,16 @@
+const name="sherlock";
+console.log(name);
+const greet=(name)=>
+{
+    console.log(`hello,${name}`);
+}
+greet("beast");
+greet("Leo");
+greet("vikram")
+globalThis.setTimeout(()=>{
+    console.log('In the timeout');
+    clearInterval(int)
+},5000);
+const int=setInterval(()=>{
+    console.log('In the every interval');
+},2000);
